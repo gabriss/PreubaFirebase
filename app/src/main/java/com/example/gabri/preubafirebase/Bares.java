bce99c054f82b44268811c22b3d9c23d54ae60fa
gabriss <gabrielramirezsegovia@gmail.com>
2017-12-05 17:16:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/gabri/preubafirebase/Bares.java
@@ -0,0 +1,32 @@
+package com.example.gabri.preubafirebase;
+
+
+public class Bares {
+    String nombre;
+    String direccion;
+    String foto;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
